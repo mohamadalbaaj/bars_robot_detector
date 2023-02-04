3b4c35be9b2177893e8a2657d6e7ee25112c26c4
mohamadalbaaj <101297059+mohamadalbaaj@users.noreply.github.com>
2023-02-05 00:23:54 +0100

    Update bar.py

--- bar.py
+++ bar.py
@@ -13,7 +13,7 @@ class Bar(Node):
 
         self.array = MarkerArray()
 
-        for i in range(1):
+        for i in range(5):
 
             self.bar = Marker()
             self.bar.header.frame_id = 'odom'
@@ -24,7 +24,37 @@ class Bar(Node):
             self.bar.action = Marker().ADD
 
             self.bar.pose.position.x = 2.0
-            self.bar.pose.position.y = i * 0.75
+            self.bar.pose.position.y = i * 0.3
+            self.bar.pose.position.z = 1.0
+
+            self.bar.pose.orientation.x = 0.0
+            self.bar.pose.orientation.y = 0.0
+            self.bar.pose.orientation.z = 0.0
+            self.bar.pose.orientation.w = 1.0
+
+            self.bar.scale.x = 1.0
+            self.bar.scale.y = 1.0
+            self.bar.scale.z = 1.0
+
+            self.bar.color.a = 1.0
+            self.bar.color.r = 0.5
+            self.bar.color.g = 0.5
+            self.bar.color.b = 0.0
+
+            self.bar.mesh_resource = "package://drobot/meshes/bar.dae"
+
+            self.array.markers.append(self.bar)
+            
+            self.bar = Marker()
+            self.bar.header.frame_id = 'odom'
+            self.bar.ns = 'bar'
+            self.bar.header.stamp = self.get_clock().now().to_msg()
+            self.bar.id = -i            
+            self.bar.type = Marker().MESH_RESOURCE
+            self.bar.action = Marker().ADD
+
+            self.bar.pose.position.x = 2.0
+            self.bar.pose.position.y = -i * 0.3
             self.bar.pose.position.z = 1.0
 
             self.bar.pose.orientation.x = 0.0
