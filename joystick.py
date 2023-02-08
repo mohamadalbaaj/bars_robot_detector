612ee44f6b184de41237e6afc1f46d1fd4540f07
mohamadalbaaj <101297059+mohamadalbaaj@users.noreply.github.com>
2023-02-08 05:21:51 +0100

    Update joystick.py

--- joystick.py
+++ joystick.py
@@ -4,7 +4,7 @@ import tf_transformations as tf_trans
 
 from rclpy.node        import Node
 from geometry_msgs.msg import Vector3, Quaternion
-from sensor_msgs.msg   import Joy
+from sensor_msgs.msg   import Joy, JointState
 from tf2_ros           import TransformStamped, TransformBroadcaster
 
 
@@ -13,6 +13,7 @@ class control_your_robot(Node):
         super().__init__(name)
 
         self.odom_broadcaster = TransformBroadcaster(self, 10)
+        self.joint_state_pub = self.create_publisher(JointState, 'joint_states', 10)
         self.joystick_sub = self.create_subscription(Joy, 'joy', self.getJoystickInput, 10)
 
         self.scale_factor = 0.02
@@ -20,19 +21,33 @@ class control_your_robot(Node):
         # Attributes to define the starting position of the robot
         self.x = 0.0
         self.y = 0.0
-        self.z = 0.0
-        self.theta = 0.0
-        self.phi = 0.0
-        self.psi = 0.0
+        self.sa = 0.0
+        self.a2h = 0.0
+        self.front_left_wheel_joint = 0.0
+        self.front_right_wheel_joint = 0.0
+        self.rear_left_wheel_joint = 0.0
+        self.front_left_wheel_joint = 0.0
+        self.bs = 0.0
+        self.a1m = 0.0
+        self.ma2 = 0.0
+        self.main = 0.0
+        self.mainn = 0.0
 
         # Base transformation odom -> base
         self.odom_trans = TransformStamped()
         self.odom_trans.header.frame_id = "odom"
         self.odom_trans.child_frame_id = "base_link"
 
+        # Definition attributes for the joints state transformations
+
+        self.jointstate = JointState()
+        self.jointstate.name = ["front_left_wheel_joint", "front_right_wheel_joint", "rear_left_wheel_joint", "rear_right_wheel_joint" ,"bs", "sa", "a1m","ma2", "a2h", "main", "mainn"]
+
     def getJoystickInput(self, msg):
         self.x += self.scale_factor * msg.axes[1]
         self.y += self.scale_factor * msg.axes[0]
+        self.sa += msg.axes[2]
+        self.a2h += msg.axes[3]
 
         self.broadcastTransformations()
 
@@ -41,17 +56,18 @@ class control_your_robot(Node):
         self.odom_trans.header.stamp = self.time_now
         self.odom_trans.transform.translation.x = self.x
         self.odom_trans.transform.translation.y = self.y
-        self.odom_trans.transform.translation.z = 0.0
 
-        q = tf_trans.quaternion_from_euler(0.0, 0.0, 0.0)
-
-        self.odom_trans.transform.rotation = Quaternion(x = q[0],y = q[1],z = q[2],w = q[3])
 
         self.odom_broadcaster.sendTransform(self.odom_trans)
 
+        self.jointstate.header.stamp = self.time_now
+        self.jointstate.position = [0.0, 0.0, 0.0, 0.0, 0.0, np.radians(self.sa), 0.0, 0.0, np.radians(self.a2h), 0.0, 0.0]
+
+        self.joint_state_pub.publish(self.jointstate)
+
 def main(args=None):
     rclpy.init(args=args)
-    control_your1_robot = control_your_robot('your_first_robot')
+    control_your1_robot = control_your_robot('joint_state_joystick')
     rclpy.spin(control_your1_robot) # equal to while true
 
 
